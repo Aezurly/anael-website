42dbe35ac8c42e33acd84f79b923143bc8871a02
Aezurly <66571923+Aezurly@users.noreply.github.com>
2023-12-28 12:00:37 -0500

    main home done + routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainProjectsComponent} from './main-projects/main-projects.component';
+import {MainHomeComponent} from './main-home/main-home.component';
 
-const routes: Routes = [];
+export const routes = [
+  { path: 'main-home-component', component: MainHomeComponent },
+  { path: 'main-projects-component', component: MainProjectsComponent },
+  { path: '',  component: MainHomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { VNavComponent } from './v-nav/v-nav.component';
+import { MainHomeComponent } from './main-home/main-home.component';
+import { MainProjectsComponent } from './main-projects/main-projects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VNavComponent
+    VNavComponent,
+    MainHomeComponent,
+    MainProjectsComponent
   ],
   imports: [
     BrowserModule,
