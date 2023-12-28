db7b80200a082dbb6101887fe63dd5d1b6536430
Aezurly <66571923+Aezurly@users.noreply.github.com>
2023-12-27 20:06:04 -0500

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VNavComponent } from './v-nav/v-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VNavComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/v-nav/v-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-v-nav',
+  templateUrl: './v-nav.component.html',
+  styleUrls: ['./v-nav.component.scss']
+})
+export class VNavComponent {
+
+}
